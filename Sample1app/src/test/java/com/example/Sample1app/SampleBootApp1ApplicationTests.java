b6de629ec222dd7832c13f9a72d7cd5c1b9fc6eb
makawamura12 <moe12121208@gmail.com>
2024-01-27 21:23:52 +0900

    first commit

---
+++ Sample1app/src/test/java/com/example/Sample1app/SampleBootApp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Sample1app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleBootApp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
